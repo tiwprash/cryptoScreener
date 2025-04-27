8683a804952b87951856f9fb014fec14e16ed293
tiwprash <135494564+tiwprash@users.noreply.github.com>
2025-04-27 21:27:40 +0530

    Update EMA_screener.py

--- EMA_screener.py
+++ EMA_screener.py
@@ -9,30 +9,27 @@ response = requests.get(url)
 symbols_data = response.json()
 
 
-def crypto_screener(symbol):
+def crypto_screener(symbol,interval,hours):
     url = "https://public.coindcx.com/market_data/candlesticks"
     now = time.time()
     current_time = int(now)
-    seconds_in_day = 60 * 60 * 24  # last 24 hours
+    seconds_in_day = 60 * 60 * hours  # last 60 hours
     start_date = now - seconds_in_day
 
     query_params = {
         "pair": symbol,
         "from": start_date,
         "to": current_time,
-        "resolution": "60",  # 15-minute candles
+        "resolution": interval,  # 15-minute candles
         "pcode": "f"
     }
+    
 
     try:
         response = requests.get(url, params=query_params)
-        if response.status_code != 200:
-            return
-
-        data = response.json().get("data", [])
-        if len(data) < 30:
-            return
-
+        if response.status_code == 200:
+            data = response.json().get("data", [])
+    
         close = np.array([float(item['close']) for item in data])
         open_ = np.array([float(item['open']) for item in data])
         low = np.array([float(item['low']) for item in data])
@@ -52,9 +49,19 @@ def crypto_screener(symbol):
         ):
             print(f"🟢 MATCH: {symbol}")
 
+        else:
+            print(f"{symbol}-- {close[-2]}")
+
     except Exception as e:
         print(f"⚠️ Error for {symbol}: {e}")
+    
 
 # Run screener for all symbols
 for symbol in symbols_data:
-    crypto_screener(symbol)
+    crypto_screener(symbol,interval="60",hours = 24)  #interval "15" or "30" or "60" or "2h" or "4h" or "1d"
+                                                        # hours should be based on Interval
+                                                        #for 60 or below hours will be 24
+                                                        #2h = 48 and 4h = 92 and 1d = 528
+                                                        #2h Interval is not working as exchange is not providing 2 hours data.
+
+print("Screening completed") # This statment will appear once the program is completed.
